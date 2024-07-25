245928c9b7b42f03133539ecae896669179380ba
Asegai <144498317+Asegai@users.noreply.github.com>
2024-07-25 15:51:30 +0530

    bug fix

--- main.py
+++ main.py
@@ -43,7 +43,8 @@ rss_urls = [
     'https://feeds.bbci.co.uk/news/tech/rss.xml'
 ]
 
-api_key = '9903ee12bcb04fcab57b84705f5a047b'
+with open('news_api_key.txt', 'r') as file:
+    api_key = file.read().strip()
 
 @app.route('/')
 def home():
